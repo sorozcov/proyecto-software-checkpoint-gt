51a33a58dfb30dc3c1ff05828c9ceac53072daa1
sorozcov <42949349+sorozcov@users.noreply.github.com>
2020-04-26 23:17:56 -0600

    Firebase Connection

---
+++ firebase/firebase.js
@@ -0,0 +1,17 @@
+// Configuracion de Firebase
+import * as firebase from "firebase";
+
+//Firebase Config del Proyecto
+var firebaseConfig = {
+  apiKey: "AIzaSyAKPue3cCi_HLt9uZE7RoZIN2alLC0HJi8",
+  authDomain: "software-checkpoint-gt.firebaseapp.com",
+  databaseURL: "https://software-checkpoint-gt.firebaseio.com",
+  projectId: "software-checkpoint-gt",
+  storageBucket: "software-checkpoint-gt.appspot.com",
+  messagingSenderId: "960829337971",
+  appId: "1:960829337971:web:4bea91361f1deaf7ca9506",
+  measurementId: "G-8RFP288CFR"
+};
+
+//Inicializar configuración de Firebase
+export default  () => firebase.initializeApp(firebaseConfig);
