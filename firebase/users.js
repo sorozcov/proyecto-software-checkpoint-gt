767b4b03a4742417150b6bb545a1285c153dbc5e
sorozcov <42949349+sorozcov@users.noreply.github.com>
2020-05-10 19:03:43 -0600

    GetUsers Firebase

--- firebase/users.js
+++ firebase/users.js
@@ -6,15 +6,23 @@
 import { firebaseFirestore } from '.';
 
 export const getUsers= async () =>{
-  
-    const users = await firebaseFirestore.collection('users').get();
- 
-    let usersArray = [];
-    await users.docs.forEach(category => {
-         usersArray.push(category.data());
-    });
-    return usersArray;
+  try{
+    const users = await db.collection('users').get();
 
+    let usersArray = [];
+    await users.docs.forEach(user => {
+        usersArray.push(user.data());
+    });
+    let usersNormalizer = {};
+    let userById ={};
+    userNormalizer['byOrder']=usersArray.map((user)=>{user.uid})
+    usersArray.map((user)=>{userById[user.uid]=user})
+    userNormalizer['byId']=userById;
+    usersArrayNormalizer['array']=usersArray;
+    return {users:usersNormalizer,error:null};
+  }catch{
+    return {users:null,error}
+  }
 }
 
 export const createUser= async (user) =>{
