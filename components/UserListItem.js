caa50e476d342343e7fd15326d01adf120b269f0
sorozcov <42949349+sorozcov@users.noreply.github.com>
2020-05-10 22:23:48 -0600

    Imagen en User Item

--- components/UserListItem.js
+++ components/UserListItem.js
@@ -3,12 +3,19 @@ import { ListItem, Left, Thumbnail, Body, Text, Right, Button } from 'native-bas
 
 import default_pic from '../src/resources/default.png';
 
+
 export default class UserListItem extends Component{
+    constructor(props){
+        super();
+        this.image = (props.image!=null ? `https://firebasestorage.googleapis.com/v0/b/software-checkpoint-gt.appspot.com/o/UserImages%2F${props.image}_400x400.jpg?alt=media` : null);
+        
+    }
     render(){
+     
         return(
             <ListItem thumbnail>
                 <Left>
-                    <Thumbnail square source={this.props.image === null ? default_pic : {uri: this.props.image}} />
+                    <Thumbnail circle source={this.image === null ? default_pic : {uri: this.image}} />
                 </Left>
                 <Body>
                     <Text>{this.props.name}</Text>
