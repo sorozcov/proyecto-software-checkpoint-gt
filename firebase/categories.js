5aa6e8b732965bc4330b777f1bc77d1d8b410c25
Luis Pedro Cuéllar <lpcuellar14@gmail.com>
2020-05-12 15:44:45 -0600

    fix firebase categorie

--- firebase/categories.js
+++ firebase/categories.js
@@ -35,54 +35,63 @@ export const getCategories= async () =>{
 
 // Funcion para crear o hacer update de un Category
 // Si es nuevo enviar categoryId=null o no enviar
-// export const updateCategory = async ({categoryId,categoryName})=>{
-//     try {
+export const updateCategory = async ({categoryId,categoryName})=>{
+    try {
 
-//         let categoryDoc = null;
-//         let isNew = categoryId==null;
-//         if(isNew){
-//           categoryDoc = await firebaseFirestore.collection(collection).doc();
-//           categoryId = categoryDoc.id;
-//         } else {
-//           categoryDoc = await firebase.firestore().collection(collection).doc(categoryId);
-//           categoryId = categoryDoc.id;
-//         }
+        let categoryDoc = null;
+        let isNew = categoryId==null;
+        if(isNew){
+          categoryDoc = await firebaseFirestore.collection(collection).doc();
+          categoryId = categoryDoc.id;
+        } else {
+          categoryDoc = await firebase.firestore().collection(collection).doc(categoryId);
+          categoryId = categoryDoc.id;
+        }
   
  
-//         let dateModified = new Date();
-//         dateModified = dateModified.getTime();
-//         let categoryInfo = {
-//             categoryId: categoryId, 
-//             categoryName: categoryName,
-//             dateModified:dateModified
-//           };
-//         if(isNew){
-//           await categoryDoc.set(categoryInfo);
-//         } else {
+        let dateModified = new Date();
+        dateModified = dateModified.getTime();
+        
+        let categoryInfo = {
+            categoryId: categoryId, 
+            categoryName: categoryName,
+            dateModified:dateModified
+        };
+        
+        if(isNew){
+          await categoryDoc.set(categoryInfo);
+        } else {
+          await categoryDoc.update(categoryInfo);
+        }
 
-//           await categoryDoc.update(categoryInfo);
-//         }
-//         return { category:categoryDoc.data(),error:null,errorMessage:null}
+        return { 
+            category: categoryDoc.data(),
+            error:null,
+            errorMessage:null
+        }
   
-//       } catch (error) {
-//         console.log("ERROR" + error.toString());
-//         let errorMessage = "No se pudo guardar la categoría."
-//         return {errorMessage:errorMessage,error,category=null}
-//       }
+    } catch (error) {
+        console.log("ERROR" + error.toString());
+        let errorMessage = "No se pudo guardar la categoría."
+        return {
+            errorMessage: errorMessage,
+            error, 
+            category: null
+        };
+    }
+}
 
-// }
+//Funcion para eliminar una categoria.
+export const deleteCategory = async ({categoryId})=>{
+    try {
 
-// //Funcion para eliminar una categoria.
-// export const deleteCategory = async ({categoryId})=>{
-//     try {
+        let categoryDoc = await firebaseFirestore.collection(collection).doc(categoryId);
+        categoryDoc = await categoryDoc.delete();
+        return { categoryId:categoryId,error:null,errorMessage:null}
+      } catch (error) {
+        console.log("ERROR" + error.toString());
+        let errorMessage = "No se pudo eliminar la categoría."
+        return {errorMessage:errorMessage,error,categoryId: null}
+      }
 
-//         let categoryDoc = await firebaseFirestore.collection(collection).doc(categoryId);
-//         categoryDoc = await categoryDoc.delete();
-//         return { categoryId:categoryId,error:null,errorMessage:null}
-//       } catch (error) {
-//         console.log("ERROR" + error.toString());
-//         let errorMessage = "No se pudo eliminar la categoría."
-//         return {errorMessage:errorMessage,error,categoryId=null}
-//       }
-
-// }
+}
